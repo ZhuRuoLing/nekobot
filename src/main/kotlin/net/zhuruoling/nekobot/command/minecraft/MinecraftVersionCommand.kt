1d51b2ad302d424055eadb57960786e7fc3e8c47
ZhuRuoLing <jkl-963@live.com>
2023-10-18 23:55:48 +0800

    fix: !mv command arg parse

--- src/main/kotlin/net/zhuruoling/nekobot/command/minecraft/MinecraftVersionCommand.kt
+++ src/main/kotlin/net/zhuruoling/nekobot/command/minecraft/MinecraftVersionCommand.kt
@@ -6,7 +6,7 @@ import net.zhuruoling.nekobot.mcversion.MinecraftVersion
 import net.zhuruoling.nekobot.message.Message
 import net.zhuruoling.nekobot.message.MessageResponse
 
-class MinecraftVersionCommand: Command() {
+class MinecraftVersionCommand : Command() {
 
     override val commandPrefix: String
         get() = "!mv"
@@ -15,7 +15,7 @@ class MinecraftVersionCommand: Command() {
         get() = "!mv Optional[<version> | latest | latestStable]"
 
     override fun handle(commandMessage: CommandMessage): Message {
-        return MessageResponse(commandMessage.scene, commandMessage.from){
+        return MessageResponse(commandMessage.scene, commandMessage.from) {
             +"**Minecraft Version**"
             +""
             val version = commandMessage[0] ?: run {
@@ -23,17 +23,26 @@ class MinecraftVersionCommand: Command() {
                 +"**Latest Snapshot Version:** ${MinecraftVersion.latestVersion}"
                 return@MessageResponse
             }
-            val minecraftVersion = versionRepository.resolve(version) ?: run {
+            val minecraftVersion = resolve(version) ?: run {
                 +"Expected version: [<version> | latest | latestStable]"
                 return@MessageResponse
             }
             val versionData = MinecraftVersion[minecraftVersion]!!
-            + "**Version:** ${versionData.id}"
+            +"**Version:** ${versionData.id}"
             +"**Version Type:** ${versionData.type}"
             +"**Release Time:** ${versionData.releaseTime}"
             +"**Version Json Download Url:** ${versionData.url}"
         }.toMessage()
     }
+
+    private fun resolve(version: String?) =
+        when (version) {
+            "latest" -> MinecraftVersion.latestVersion
+            "latestStable" -> MinecraftVersion.latestStableVersion
+            null -> null
+            else -> if (version in MinecraftVersion.versions.keys) version else null
+        }
+
 
     override fun prepare() {
     }
